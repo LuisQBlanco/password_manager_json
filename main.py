23b65248c39eda612230d7e7b1fefd4e72d07c0f
LuisQBlanco <ingluisq@gmail.com>
2022-01-17 13:59:08 -0500

    Read, update and write json file

--- main.py
+++ main.py
@@ -45,11 +45,18 @@ def save():
 
         # if is_ok:
         with open("data.json", "r") as data_file:
-            # data_file.write(f"{website} | {email} | {password}\n")
-            # json.dump(new_data, data_file, indent=4)
+            # Reading old data
             data = json.load(data_file)
+            # Updating old data with new data
+            data.update(new_data)
             print(data)
             print(type(data))
+
+        with open("data.json", "w") as data_file:
+            # Saving update data
+            json.dump(data, data_file, indent=4)
+
+
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
